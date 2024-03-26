530d4f3ff30df5dac3178da5c801458146021907
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-03-26 13:57:22 +0100

    added class to gamesList

--- js/games.js
+++ js/games.js
@@ -2,6 +2,7 @@ export function displayGamesList() {
   const gamesContainer = document.querySelector('#gamesContainer');
 
   let gamesList = document.createElement('ul');
+  gamesList.classList.add('gamesList');
   gamesList.innerHTML = `
   <li>Elder Scrolls Online</li>
   <li>Baldur's Gate 3</li>
