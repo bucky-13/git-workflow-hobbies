7239c3979bccafb3c5a4d63b960701794d94fc86
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-03-28 10:09:12 +0100

    created displayAboutSection function

---
+++ js/about.js
@@ -0,0 +1,9 @@
+const app = document.querySelector('#app');
+
+export function displayAboutSection() {
+  app.innerHTML = '';
+  let abouth2 = document.createElement('h2');
+  h2.textContent = 'About this page';
+
+  app.append(abouth2);
+}
