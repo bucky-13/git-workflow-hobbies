c27be7d65fdefdcc4f8fda8304b814ae51c7bf0d
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-03-28 09:36:35 +0100

    added id for sections in html and querySelectors in main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import './style.css';
 import { displayGamesList } from './js/games.js';
 
+const displayGamesSection = document.querySelector('displayGamesSection');
+const displayAboutSection = document.querySelector('displayAboutSection');
+
 document.querySelector('#app').innerHTML = `
   <div class="gamesContainer" id="gamesContainer">
     <h2>List of Games</h2>
